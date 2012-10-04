4399a8c112f1f8d90d8f22939e24a2177bdaab66
Issac Trotts <itrotts@willowgarage.com>
2012-10-03 20:00:18 -0700

    Split test_remove_install_check into two tests.

---
+++ test_remove_check.bash
@@ -0,0 +1,3 @@
+yes | xylem remove $package
+should_fail xylem check $package
+
--- test_remove_install_check.bash
+++
@@ -1,9 +0,0 @@
-# Test that removing, checking and installing all work together.
-# This test should probably be run under sudo.
-
-package=fltk
-yes | xylem remove $package
-should_fail xylem check $package
-yes | xylem install $package
-xylem check $package
-
