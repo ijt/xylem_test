2ca9bd22c1b1d5e50730ea689ca07e93201fe196
Issac Trotts <itrotts@willowgarage.com>
2012-10-03 20:04:16 -0700

    Add a comment.

--- test_resolve.bash
+++ test_resolve.bash
@@ -1,2 +1,5 @@
+# Every line in these tests has to resolve to true (non-zero status code). So if
+# xylem resolve fltk doesn't output a string containing 'fltk' then the next
+# line will evaluate to non-true and the test will fail.
 [[ $(xylem resolve fltk) = *fltk* ]]
 
